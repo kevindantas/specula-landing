d0a67cada2d3c398acd64175e8e5d3f0507f37e8
Wendel Nascimento <wendel.nascimento2@outlook.com>
2017-06-04 03:29:08 -0300

    gradiente quase animando daora

--- src/App.js
+++ src/App.js
@@ -6,12 +6,45 @@ import FirstSlide from './components/FirstSlide';
 import Team from './components/Team';
 
 class App extends Component {
+  state = {
+    scrollClass: '',
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', event => this.handleScroll(event));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll');
+  }
+
+  handleScroll(event) {
+    console.log(event);
+    const scroll = event.target.scrollingElement.scrollTop;
+    if (scroll < 30) {
+      this.setState({
+        scrollClass: '',
+      });
+    } else if (scroll >= 30) {
+      this.setState({
+        scrollClass: 'section1',
+      });
+    } else if (scroll >= 60) {
+      this.setState({
+        scrollClass: 'section2',
+      });
+    } else {
+      this.setState({
+        scrollClass: 'section3',
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-
-        <main>
+        <main className={this.state.scrollClass}>
           <FirstSlide />
           <Team />
         </main>
